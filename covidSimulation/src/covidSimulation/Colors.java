0cd7a67e7b55b037a9d91ac2d0cbd1bd1109fce6
iiitttsss <67986176+iiitttsss@users.noreply.github.com>
2021-09-02 08:50:23 -0400

    Delete covidSimulation/src directory

--- covidSimulation/src/covidSimulation/Colors.java
+++
@@ -1,8 +0,0 @@
-package covidSimulation;
-
-public class Colors
-{
-	public static final int AGENT_COLOR_HEALTHY = Global.getPro().color(100, 200, 230);
-	public static final int AGENT_COLOR_SICK = Global.getPro().color(255, 140, 140);
-
-}
